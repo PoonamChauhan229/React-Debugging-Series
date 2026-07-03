708a1c5292e8bf9a494cea65376a0e3acb5cee71
Poonam229 <poonam.chauhan229@gmail.com>
2026-07-03 22:55:24 +0530

    bug10

--- src/components/bug10-memory-leak/NotiifcationCounter.jsx
+++ src/components/bug10-memory-leak/NotiifcationCounter.jsx
@@ -1,23 +1,28 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 
-const NotificationCounter = ({
-    notifications,
-    setNotifications,
-}) => {
+const NotificationCounter = () => {
+
+    const [notifications, setNotifications] = useState(0);
     useEffect(() => {
         console.log("NotificationCounter Mounted");
 
         const interval = setInterval(() => {
-            console.log("Fetching new notifications ,interval is still running...");
+            console.log("Fetching new notifications ,interval is still running in background...");
             setNotifications((prev) => prev + 1);
         }, 1000);
+        return ()=>{
+            console.log("Cleaning up the interval")
+            clearInterval(interval)
+        }
     }, []);
 
     return (
         <div className="notification-card">
             <h4>Live Notifications</h4>
 
-            <p className="count">{notifications}</p>
+            <div className="center-circle">
+                <p className="count">{notifications}</p>
+            </div>
 
             <p className="note">
                 Open the browser console and click
